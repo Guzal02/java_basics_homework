4e87c33a62f7ab6cd18a48e72c2254f9e62df968
Guzal <02guzal@gmail.com>
2021-05-09 02:20:47 +0300

    обновила

--- 06_NumbersStringsAndDates_with_tests_14_01_21/06_NumbersStringsAndDates/homework_6.4/MaskingInformation/src/main/java/Main.java
+++ 06_NumbersStringsAndDates_with_tests_14_01_21/06_NumbersStringsAndDates/homework_6.4/MaskingInformation/src/main/java/Main.java
@@ -1,23 +1,23 @@
 public class Main {
-
     public static void main(String[] args) {
         System.out.println(searchAndReplaceDiamonds("Номер кредитной карты <4008 1234 5678> 8912", "***"));
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
-
         int openingAngleBracketIndex = 0;
         int closingAngleBracketIndex = 0;
         String safe = "";
 
-        for (int i = 0; i < text.length(); i++) {
+        for (int i = 0; i < text.length(); ++i) {
             char chrs = text.charAt(i);
             if (chrs == '<') {
                 openingAngleBracketIndex = text.indexOf(chrs);
             }
+
             if (chrs == '>') {
                 closingAngleBracketIndex = text.indexOf(chrs);
             }
+
             if (closingAngleBracketIndex > 0) {
                 safe = text.substring(openingAngleBracketIndex, closingAngleBracketIndex + 1);
                 text = text.replace(safe, placeholder);
@@ -27,5 +27,4 @@ public class Main {
         }
         return text;
     }
-
 }
