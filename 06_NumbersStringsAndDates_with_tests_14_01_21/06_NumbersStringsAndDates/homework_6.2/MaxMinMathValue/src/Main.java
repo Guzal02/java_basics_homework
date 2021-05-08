9018796407e91547ae48dda0b97c587670cd37b8
Guzal <02guzal@gmail.com>
2021-05-09 01:18:49 +0300

    исправила домашкку 6.2

--- 06_NumbersStringsAndDates_with_tests_14_01_21/06_NumbersStringsAndDates/homework_6.2/MaxMinMathValue/src/Main.java
+++ 06_NumbersStringsAndDates_with_tests_14_01_21/06_NumbersStringsAndDates/homework_6.2/MaxMinMathValue/src/Main.java
@@ -9,8 +9,8 @@ public class Main {
         System.out.println("Min значение типа числа byte: " + Byte.MIN_VALUE);
         System.out.println("Max значение типа числа byte: " + Byte.MAX_VALUE);
         System.out.println("Min значение типа числа double: " + Double.MIN_VALUE);
-        System.out.println("Max значение типа числа double: " + Double.MAX_VALUE);
+        System.out.println("Max значение типа числа double: " + -Double.MAX_VALUE);
         System.out.println("Min значение типа числа float: " + Float.MIN_VALUE);
-        System.out.println("Max значение типа числа float: " + Float.MAX_VALUE);
+        System.out.println("Max значение типа числа float: " + -Float.MAX_VALUE);
     }
 }
